67dd01262775abb9aa85e850a7a1579117a4d19d
Pawel Wodyk <pawlosik@yahoo.com>
2019-05-21 16:50:09 +0000

    added contact page

--- run.py
+++ run.py
@@ -9,6 +9,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+    
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
+@app.route("/about")
+def contact(): 
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
