fda2aff2ace331653925ff9267215dddf4b34eea
Pawel Wodyk <pawlosik@yahoo.com>
2019-05-21 17:15:05 +0000

    completed the getting started with flask section

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 import os
 from flask import Flask, render_template
 
-app = Flask
-
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,10 +11,19 @@ def index():
 def about():
     return render_template("about.html")
     
-@app.route("/about")
+@app.route("/contact")
 def contact(): 
     return render_template("contact.html")
+    
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
+    """
+    configuration used to host the website in the cloud9
+    """
+    app.run(host=os.environ.get("IP"), 
+            port=int(os.environ.get("PORT")), 
+            debug=True)
